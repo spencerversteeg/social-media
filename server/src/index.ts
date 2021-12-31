2c86fd6b1d86b1dcea5fa1d6be44b5a44d360133
Spencer Versteeg <versteeg.spencer@gmail.com>
2021-12-31 10:31:29 -0500

    Added cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,32 @@ import 'dotenv/config';
 
 // Package imports
 import express, { Application } from 'express';
+import cors, { CorsOptions } from 'cors';
+
+const corsWhitelist = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://localhost:8080',
+  'http://localhost:8081',
+  'http://localhost:5432',
+  'production-domain-here.com'
+];
+
+const corsOptions: CorsOptions = {
+  origin(origin, callback) {
+    if (
+      // if theyre hitting from none browser
+      !origin ||
+      corsWhitelist.indexOf(origin) !== -1
+    ) {
+      callback(null, true);
+    } else {
+      callback(null, false);
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 // Project imports
 import router from './routes';
@@ -15,6 +41,7 @@ validateEnv();
 const app: Application = express();
 
 // Initialize middleware
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/api', router);
